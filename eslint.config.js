78909ef5715b08a07f430b9e481f5347188ec2b6
chan9yu <dev.cgyeo@gmail.com>
2026-07-07 16:46:40 +0900

    chore: first commit

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+	globalIgnores(['dist']),
+	{
+		files: ['**/*.{js,jsx,ts,tsx}'],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+			prettierConfig
+		],
+		plugins: {
+			'simple-import-sort': simpleImportSort
+		},
+		languageOptions: {
+			globals: globals.browser
+		},
+		rules: {
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'error'
+		}
+	}
+]);
