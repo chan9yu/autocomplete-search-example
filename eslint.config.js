3ce8574698bacd8ce0144aae54d8e24dc3d14098
chan9yu <dev.cgyeo@gmail.com>
2026-07-09 14:03:14 +0900

    style: prettier 적용

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import js from '@eslint/js';
-import { defineConfig, globalIgnores } from 'eslint/config';
-import prettierConfig from 'eslint-config-prettier';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettierConfig from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-	globalIgnores(['dist']),
+	globalIgnores(["dist"]),
 	{
-		files: ['**/*.{js,jsx,ts,tsx}'],
+		files: ["**/*.{js,jsx,ts,tsx}"],
 		extends: [
 			js.configs.recommended,
 			tseslint.configs.recommended,
@@ -19,14 +19,14 @@ export default defineConfig([
 			prettierConfig
 		],
 		plugins: {
-			'simple-import-sort': simpleImportSort
+			"simple-import-sort": simpleImportSort
 		},
 		languageOptions: {
 			globals: globals.browser
 		},
 		rules: {
-			'simple-import-sort/imports': 'error',
-			'simple-import-sort/exports': 'error'
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error"
 		}
 	}
 ]);
